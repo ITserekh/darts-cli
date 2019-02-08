c82833acaa60eddf92a3ea306e387366f0dc62d8
Ivan Tserakh <tserekh.ivan@gmail.com>
2019-02-08 09:24:37 +0300

    display data of some table

--- src/app/components/show-table/show-table.component.ts
+++ src/app/components/show-table/show-table.component.ts
@@ -1,12 +1,5 @@
 import {Component, ContentChild, OnInit, TemplateRef, Input } from '@angular/core';
-// import { USER2_URL} from '../../services/urls';
-// import { GetDataService } from '../../services/get-data.service';
-import { Users2 } from '../../services/users';
 import { Observable } from 'rxjs';
-/*
-import { UserAgeDirective, UserAvatarDirective,
-  UserDateDirective, UserNameDirective, UserIdDirective} from '../../services/table-users.directive';
-  */
 import {TableSetting} from '../../services/table-configs/setting-table';
 
 
@@ -16,13 +9,6 @@ import {TableSetting} from '../../services/table-configs/setting-table';
   styleUrls: ['./show-table.component.scss']
 })
 export class ShowTableComponent implements OnInit {
-  /*
-  @ContentChild(UserIdDirective, {read: TemplateRef}) userIdTemplate;
-  @ContentChild(UserNameDirective, {read: TemplateRef}) userNameTemplate;
-  @ContentChild(UserAgeDirective, {read: TemplateRef}) userAgeTemplate;
-  @ContentChild(UserDateDirective, {read: TemplateRef}) userDateTemplate;
-  @ContentChild(UserAvatarDirective, {read: TemplateRef}) userAvatarTemplate;
-  */
 
   @Input() tableSetting: Observable<TableSetting>;
 
