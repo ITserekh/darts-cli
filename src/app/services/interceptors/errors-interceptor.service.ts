b1bf6a58117662edbce5897cbecbe7959d8976e4
Ivan Tserakh <tserekh.ivan@gmail.com>
2019-02-19 09:35:38 +0300

    refactor support the 10001 error

--- src/app/services/interceptors/errors-interceptor.service.ts
+++ src/app/services/interceptors/errors-interceptor.service.ts
@@ -1,5 +1,12 @@
 import { Injectable } from '@angular/core';
-import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest } from '@angular/common/http';
+import {
+  HttpEvent,
+  HttpInterceptor,
+  HttpHandler,
+  HttpRequest,
+  HttpResponse,
+  HttpErrorResponse
+} from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 import {Router} from '@angular/router';
@@ -14,6 +21,24 @@ export class ErrorsInterceptorService implements HttpInterceptor {
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     return next.handle(request).pipe(catchError(err => {
+      if (err instanceof HttpErrorResponse) {
+        if (err.error.errorInfo.error.indexOf('10001') > -1 ) {
+          window.alert('зарегистрируйтесь');
+        }
+      }
+      return throwError(err);
+
+    }), (event: Observable<HttpEvent<any>>) => {
+        event.subscribe(item => {
+          if (item instanceof HttpResponse) {
+            // console.log(item.body);
+            // console.log(item.headers);
+          }});
+      return event;
+    }
+      );
+    /*
+    return next.handle(request).pipe(catchError(err => {
 
       this.router.navigate(['/login']);
       const error = err.error.message || err.statusText;
@@ -21,5 +46,6 @@ export class ErrorsInterceptorService implements HttpInterceptor {
       console.log(error);
       return throwError(error);
     }));
+    */
   }
 }
